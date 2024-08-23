0f0032e4d6abbe1205a978d24d6fd0de07ae2846
Nicolas <nicolasgghg12@gmail.com>
2024-08-23 10:50:28 -0300

    Adicionado a rota tabuada

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ app.get('/teste',(req,res)=>{
     res.json({msg:"teste", msg2:"teste2", msg3:"teste3"})
 })
 
+app.get('/tabuada',(req,res)=>{
+    let numDaTabuada = 3
+    let respEnviarFinal = ""
+    for (let index = 0; index < 10; index++) {
+        respEnviarFinal += `<center>|  ${numDaTabuada} x ${index} = ${numDaTabuada*index} | </center> <br>`
+    }
+
+    res.send(respEnviarFinal)
+})
+
 app.listen(port, ()=>{
     console.log(`API Rodando em http://localgost:${port}`);
 })
